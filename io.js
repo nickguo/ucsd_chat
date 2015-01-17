e1fc1a7e58e98adcef95795f7a1533e7fc865413
nickguo <nick.n.guo@gmail.com>
2015-01-17 00:16:07 -0800

    chat room switching working

--- io.js
+++ io.js
@@ -5,30 +5,36 @@ var debug = require('debug')('chat');
 // create socketIO server
 var io = new SocketIO();
 
-// usernames which are currently connected to the chat
-var usernames = {};
-var numUsers = 0;
+// usernames which are currently connected to the chat rooms
+var usernames = {}; // double dictionary
+var numUsers = {};  // dictionary
+var getRooms = {};
 
 
 io.on('connection', function (socket) {
   var addedUser = false;
   debug ("new connection: " + socket.id +
-          "\n\t on ip: " + socket.handshake.address + 
-          "\n\t on room: " + socket.room);
+          "\n\t on ip: " + socket.handshake.address);
 
 
   // this function fires immediately after a connection
   socket.on('add topic', function(data) {
+    //console.log("GOT ADD TOPIC");
     // remove all whitespace from the room data
     var room = data.topic.replace(/ /g,"");
     socket.join(room);
     debug("socket on ip: " + socket.handshake.address + " joined: " + room);
+    debug("socket on room: " + room);
+    getRooms[socket.id] = room;
+
+    //console.dir[getRooms];
   });
 
   // when the client emits 'new message', this listens and executes
   socket.on('new message', function (data) {
     // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
+    socket.room = getRooms[socket.id];
+    socket.broadcast.to(socket.room).emit('new message', {
       username: socket.username,
       message: data
     });
@@ -36,47 +42,71 @@ io.on('connection', function (socket) {
 
   // when the client emits 'add user', this listens and executes
   socket.on('add user', function (username) {
+    //console.log("GOT ADD USER");
+    socket.room = getRooms[socket.id];
+    // handle the case of the first user
+    if (usernames[socket.room] == undefined) {
+      usernames[socket.room] = {};
+    }
+    if (numUsers[socket.room] == undefined) {
+      numUsers[socket.room] = 0;
+    }
+
+    debug("ROOM IN ADD USER IS: " + socket.room);
+
     // we store the username in the socket session for this client
     socket.username = username;
     // add the client's username to the global list
-    usernames[username] = username;
-    ++numUsers;
+    usernames[socket.room][socket.username] = socket.username;
+
+    //console.log("room is: " + socket.room);
+    //console.dir(usernames);
+
+    //usernames[username] = username;
+    usernames[usernames.length] = username;
+
+    ++(numUsers[socket.room]);
     addedUser = true;
     socket.emit('login', {
-      numUsers: numUsers
+      numUsers: numUsers[socket.room]
     });
     // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
+    socket.broadcast.to(socket.room).emit('user joined', {
       username: socket.username,
-      numUsers: numUsers
+      numUsers: numUsers[socket.room]
     });
   });
 
   // when the client emits 'typing', we broadcast it to others
   socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
+    socket.room = getRooms[socket.id];
+    socket.broadcast.to(socket.room).emit('typing', {
       username: socket.username
     });
   });
 
   // when the client emits 'stop typing', we broadcast it to others
   socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
+    socket.room = getRooms[socket.id];
+    socket.broadcast.to(socket.room).emit('stop typing', {
       username: socket.username
     });
   });
 
   // when the user disconnects.. perform this
   socket.on('disconnect', function () {
+    socket.room = getRooms[socket.id];
     // remove the username from global usernames list
     if (addedUser) {
-      delete usernames[socket.username];
-      --numUsers;
+      debug("trying to delete user: " + socket.username + " - from: " + socket.room);
+      debug("\t with username: " + usernames[socket.room]);
+      delete usernames[socket.room][socket.username];
+      --(numUsers[socket.room]);
 
       // echo globally that this client has left
-      socket.broadcast.emit('user left', {
+      socket.broadcast.to(socket.room).emit('user left', {
         username: socket.username,
-        numUsers: numUsers
+        numUsers: numUsers[socket.room]
       });
     }
   });
